321af5d82e228dc8dba9858b4b9417c41ade0bb2
Stephen Freiberg <safreiberg@gmail.com>
2016-01-17 23:11:35 -0800

    Add result endpoint

--- src/main/java/com/augurworks/alfred/server/AlfredEndpoint.java
+++ src/main/java/com/augurworks/alfred/server/AlfredEndpoint.java
@@ -16,50 +16,73 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AlfredEndpoint {
-	
-	private static final Logger log = LoggerFactory.getLogger(AlfredEndpoint.class);
-	private AlfredPrefs prefs;
-	
-	public void setPrefs(AlfredPrefs prefs) {
-		this.prefs = prefs;
-	}
 
-    @RequestMapping(value="/get/{id}", method = RequestMethod.GET, produces = "application/json")
-    public @ResponseBody String getStatus(@PathVariable String id) {
-        File fileForId = getFileForId(id);
-        if (fileForId.exists()) {
-        	try {
-				return FileUtils.readFileToString(fileForId);
-			} catch (IOException e) {
-				log.error("Unable to read file " + fileForId, e);
-				return null;
-			}
+    private static final Logger log = LoggerFactory.getLogger(AlfredEndpoint.class);
+    private AlfredPrefs prefs;
+
+    public void setPrefs(AlfredPrefs prefs) {
+        this.prefs = prefs;
+    }
+
+    public enum TrainStatus {
+        UNKNOWN,
+        IN_PROGRESS,
+        COMPLETE;
+    }
+
+    @RequestMapping(value="/status/{id}", method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody TrainStatus getStatus(@PathVariable String id) {
+        File trainFileForId = getTrainFileForId(id);
+        File outFileForId = getOutputFileForId(id);
+        if (outFileForId.exists()) {
+            return TrainStatus.COMPLETE;
+        } else if (trainFileForId.exists()) {
+            return TrainStatus.IN_PROGRESS;
         } else {
-        	return null;
+            return TrainStatus.UNKNOWN;
         }
     }
-    
-    @RequestMapping(value="/train", method = RequestMethod.POST, produces = "application/json")
-    public @ResponseBody String train(@RequestBody String net) {
-    	String id = UUID.randomUUID().toString();
-    	File fileForId = getFileForId(id);
-    	try {
-			FileUtils.writeStringToFile(fileForId, net);
-			return id;
-		} catch (IOException e) {
-			log.error("Unable to write file " + fileForId + " with contents:\n" + net, e);
-			throw new IllegalStateException("Unable to store net.");
-		}
+
+    @RequestMapping(value="/result/{id}", method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody String getResult(@PathVariable String id) {
+        TrainStatus status = getStatus(id);
+        if (status != TrainStatus.COMPLETE) {
+            throw new IllegalArgumentException("Unable to return result for ID " + id + ", status was " + status);
+        }
+        File outFileForId = getOutputFileForId(id);
+        try {
+            return FileUtils.readFileToString(outFileForId);
+        } catch (IOException e) {
+            log.error("Unable to read result file for ID " + id, e);
+            throw new IllegalStateException("Unable to return result for ID " + id + ", status was " + status);
+        }
     }
 
-	private File getFileForId(String id) {
-		return new File(prefs.getDirectory() + "/" + id);
-	}
+    @RequestMapping(value="/train", method = RequestMethod.POST, produces = "application/json")
+    public @ResponseBody String train(@RequestBody String net) {
+        String id = UUID.randomUUID().toString();
+        File fileForId = getTrainFileForId(id);
+        try {
+            FileUtils.writeStringToFile(fileForId, net);
+            return id;
+        } catch (IOException e) {
+            log.error("Unable to write file " + fileForId + " with contents:\n" + net, e);
+            throw new IllegalStateException("Unable to store net.");
+        }
+    }
+
+    private File getTrainFileForId(String id) {
+        return new File(prefs.getDirectory() + "/" + id + ".augtrain");
+    }
+
+    private File getOutputFileForId(String id) {
+        return new File(prefs.getDirectory() + "/" + id + ".augout");
+    }
 
     @RequestMapping(value="/hello", method = RequestMethod.GET, produces = "application/json")
     public @ResponseBody String hello() {
-    	System.out.println("hello!");
-    	return "{\"message\":\"Hello, world!\"}";
+        System.out.println("hello!");
+        return "{\"message\":\"Hello, world!\"}";
     }
 
 }
