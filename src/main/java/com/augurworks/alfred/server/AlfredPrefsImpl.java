e9185a63d252bbc552b733020deebf95ac8a085b
TheConnMan <bcconn2112@gmail.com>
2016-02-21 13:08:16 -0500

    Change preferences to use environment variables

--- src/main/java/com/augurworks/alfred/server/AlfredPrefsImpl.java
+++ src/main/java/com/augurworks/alfred/server/AlfredPrefsImpl.java
@@ -1,21 +1,14 @@
 package com.augurworks.alfred.server;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
-import org.apache.commons.io.IOUtils;
+import com.augurworks.alfred.scaling.ScaleFunctions.ScaleFunctionType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.augurworks.alfred.scaling.ScaleFunctions.ScaleFunctionType;
+import java.io.File;
 
 public class AlfredPrefsImpl implements AlfredPrefs {
 
     private static final Logger log = LoggerFactory.getLogger(AlfredPrefsImpl.class);
-    private static final String FILENAME = "alfred.prefs";
     private static final String DIRECTORY_PREF = "LISTEN_DIRECTORY";
     private static final String DIRECTORY_DEFAULT = "nets";
     private static final String NUM_THREADS_PREF = "NUM_THREADS";
@@ -25,69 +18,34 @@ public class AlfredPrefsImpl implements AlfredPrefs {
     private static final String SCALE_FUNC_PREF = "SCALE_FUNCTION";
     private static final String SCALE_FUNC_DEFAULT = "SIGMOID";
 
-    private Properties reload() {
-        File file = new File(FILENAME);
-        if (file.exists()) {
-            return reloadFromFile();
-        }
-        return reloadFromClasspath();
-    }
-
-    private Properties reloadFromClasspath() {
-        Properties properties = new Properties();
-        InputStream in = null;
-        try {
-            in = this.getClass().getClassLoader()
-                    .getResourceAsStream(FILENAME);
-            properties.load(in);
-        } catch (IOException e) {
-            log.error("Unable to read prefs from classpath " + FILENAME, e);
-        } finally {
-            IOUtils.closeQuietly(in);
-        }
-        return properties;
-    }
-
-    private Properties reloadFromFile() {
-        Properties properties = new Properties();
-        FileInputStream in = null;
-        try {
-            in = new FileInputStream(FILENAME);
-            properties.load(in);
-        } catch (IOException e) {
-            log.error("Unable to read prefs file " + FILENAME, e);
-        } finally {
-            IOUtils.closeQuietly(in);
-        }
-        return properties;
-    }
-
     @Override
     public String getDirectory() {
-        return new File(reload().getProperty(DIRECTORY_PREF, DIRECTORY_DEFAULT)).getAbsolutePath();
+        String environmentPath = System.getProperty(DIRECTORY_PREF) == null ? System.getenv(DIRECTORY_PREF) : System.getProperty(DIRECTORY_PREF);
+        return new File(environmentPath == null ? DIRECTORY_DEFAULT : environmentPath).getAbsolutePath();
     }
 
     @Override
     public int getNumThreads() {
-        return parseInt(reload().getProperty(NUM_THREADS_PREF, NUM_THREADS_DEFAULT), NUM_THREADS_DEFAULT);
+        String environmentPath = System.getProperty(NUM_THREADS_PREF) == null ? System.getenv(NUM_THREADS_PREF) : System.getProperty(NUM_THREADS_PREF);
+        return parseInt(environmentPath, NUM_THREADS_DEFAULT);
     }
 
     @Override
     public int getTimeout() {
-        return parseInt(reload().getProperty(TIMEOUT_PREF, TIMEOUT_DEFAULT), TIMEOUT_DEFAULT);
+        String environmentPath = System.getProperty(TIMEOUT_PREF) == null ? System.getenv(TIMEOUT_PREF) : System.getProperty(TIMEOUT_PREF);
+        return parseInt(environmentPath, TIMEOUT_DEFAULT);
     }
 
     @Override
     public ScaleFunctionType getScaleFunction() {
-        return ScaleFunctionType.fromString(reload().getProperty(SCALE_FUNC_PREF, SCALE_FUNC_DEFAULT));
+        String environmentPath = System.getProperty(SCALE_FUNC_PREF) == null ? System.getenv(SCALE_FUNC_PREF) : System.getProperty(SCALE_FUNC_PREF);
+        return ScaleFunctionType.fromString(environmentPath == null ? SCALE_FUNC_DEFAULT : environmentPath);
     }
 
     private int parseInt(String pref, String defaultValue) {
         try {
             return Integer.parseInt(pref);
-        } catch (NumberFormatException e) {
-            log.error("Unable to parse integer from " + pref, e);
-        }
+        } catch (NumberFormatException e) { }
         // this should throw if it fails.
         return Integer.parseInt(defaultValue);
     }
