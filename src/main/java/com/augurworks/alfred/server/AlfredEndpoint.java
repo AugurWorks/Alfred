5f90ad330476c822d9d7477a6e77d9d59f24a4fa
TheConnMan <bcconn2112@gmail.com>
2016-05-21 15:35:53 -0400

    Update log endpoint to return plain text logs

--- src/main/java/com/augurworks/alfred/server/AlfredEndpoint.java
+++ src/main/java/com/augurworks/alfred/server/AlfredEndpoint.java
@@ -1,7 +1,5 @@
 package com.augurworks.alfred.server;
 
-import org.apache.commons.io.FileUtils;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +13,9 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 import springfox.documentation.annotations.ApiIgnore;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Collection;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.UUID;
 
 @Controller
@@ -60,28 +58,8 @@ public class AlfredEndpoint {
     }
 
     @RequestMapping(value = "/logs/{id}", method = RequestMethod.GET, produces = "application/json")
-    public @ResponseBody String getLogs(@PathVariable String id) {
-        Collection<File> logFiles = getLogsForId(id);
-        JSONObject object = new JSONObject();
-        for (File file : logFiles) {
-            addFile(object, file);
-        }
-        return object.toString(2);
-    }
-
-    private void addFile(JSONObject object, File file) {
-        try {
-            if (file.isFile()) {
-                object.put(file.getName(), FileUtils.readFileToString(file));
-            }
-        } catch (IOException e) {
-            log.error("Unable to read file " + file + ". Skipping it and moving on.", e);
-            object.put(file.getName(), "ERROR: Unable to read file: " + e.getMessage());
-        }
-    }
-
-    private Collection<File> getLogsForId(String id) {
-        return FileUtils.listFiles(new File("logs/" + id + "/"), null /* accept all file extensions */, true /* recurse */);
+    public @ResponseBody String getLogs(@PathVariable String id) throws IOException {
+        return new String(Files.readAllBytes(Paths.get("logs/" + id + ".log")));
     }
 
     @ApiIgnore
