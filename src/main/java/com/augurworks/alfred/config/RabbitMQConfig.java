eaf8a501e3d6133126dc8da63f3781cff39b1b04
TheConnMan <bcconn2112@gmail.com>
2016-06-10 20:59:30 -0400

    Add correct external RabbitMQ config variables

--- src/main/java/com/augurworks/alfred/config/RabbitMQConfig.java
+++ src/main/java/com/augurworks/alfred/config/RabbitMQConfig.java
@@ -3,7 +3,9 @@ package com.augurworks.alfred.config;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,28 +15,45 @@ import java.util.concurrent.TimeoutException;
 @Configuration
 public class RabbitMQConfig {
 
+    Logger log = LoggerFactory.getLogger(RabbitMQConfig.class);
+
+    @Value("${rabbitmq.username}")
+    private String username;
+
+    @Value("${rabbitmq.password}")
+    private String password;
+
+    @Value("${rabbitmq.hostname}")
+    private String hostname;
+
+    @Value("${rabbitmq.port}")
+    private Integer port;
+
     public static final String TRAINING_CHANNEL = "nets.training";
     public static final String RESULTS_CHANNEL = "nets.results";
 
-    private final Connection connection;
-
-    @Autowired
-    public RabbitMQConfig() throws IOException, TimeoutException {
-        ConnectionFactory factory = new ConnectionFactory();
-        this.connection = factory.newConnection();
-    }
-
     @Bean
-    public Channel trainingChannel() throws IOException {
-        Channel channel = this.connection.createChannel();
+    public Channel trainingChannel() throws IOException, TimeoutException {
+        log.info("Creating training RabbitMQ channel");
+        Channel channel = getConnection().createChannel();
         channel.queueDeclare(TRAINING_CHANNEL, false, false, false, null);
         return channel;
     }
 
     @Bean
-    public Channel resultsChannel() throws IOException {
-        Channel channel = this.connection.createChannel();
+    public Channel resultChannel() throws IOException, TimeoutException {
+        log.info("Creating results RabbitMQ channel");
+        Channel channel = getConnection().createChannel();
         channel.queueDeclare(RESULTS_CHANNEL, false, false, false, null);
         return channel;
+    }
+
+    private Connection getConnection() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername(username);
+        factory.setPassword(password);
+        factory.setHost(hostname);
+        factory.setPort(port);
+        return factory.newConnection();
     }
 }
