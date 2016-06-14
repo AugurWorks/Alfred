23b7310d71ec83d7944de5f68f8093087c9721bb
TheConnMan <bcconn2112@gmail.com>
2016-06-13 21:22:47 -0400

    Remove getters and constructors with Lombok

--- src/main/java/com/augurworks/alfred/TimingInfo.java
+++ src/main/java/com/augurworks/alfred/TimingInfo.java
@@ -1,14 +1,12 @@
 package com.augurworks.alfred;
 
+import lombok.Data;
+
+@Data
 public class TimingInfo {
 
     private final long startTime;
     private final long maxAllowedDuration;
-
-    public TimingInfo(long startTime, long maxAllowedDuration) {
-        this.startTime = startTime;
-        this.maxAllowedDuration = maxAllowedDuration;
-    }
 
     public static TimingInfo withDuration(long maxAllowedDuration) {
         return new TimingInfo(System.currentTimeMillis(), maxAllowedDuration);
@@ -21,9 +19,4 @@ public class TimingInfo {
             return System.currentTimeMillis() > (startTime + maxAllowedDuration);
         }
     }
-
-    public long getStartTime() {
-        return startTime;
-    }
-
 }
