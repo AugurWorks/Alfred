1592eb08bf24fbc5edd36e4307b2ef5b78eb0e42
TheConnMan <bcconn2112@gmail.com>
2016-06-18 16:06:40 -0400

    Add dev Docker tagging script

--- docker-tag.sh
+++ docker-tag.sh
@@ -2,11 +2,11 @@
 
 version=`cat build.gradle | grep "^version " | sed -r "s/.*version = '(.*)'$/\1/"`
 
-echo "Building container v$version"
+echo "Building container v$version$1"
 docker build -t alfred .
 
-echo "Tagging version $version"
-docker tag alfred 274685854631.dkr.ecr.us-east-1.amazonaws.com/alfred:$version
-docker tag alfred 274685854631.dkr.ecr.us-east-1.amazonaws.com/alfred:latest
-docker push 274685854631.dkr.ecr.us-east-1.amazonaws.com/alfred:$version
-docker push 274685854631.dkr.ecr.us-east-1.amazonaws.com/alfred:latest
+echo "Tagging version $version$1"
+docker tag alfred 274685854631.dkr.ecr.us-east-1.amazonaws.com/alfred:$version$1
+docker tag alfred 274685854631.dkr.ecr.us-east-1.amazonaws.com/alfred:latest$1
+docker push 274685854631.dkr.ecr.us-east-1.amazonaws.com/alfred:$version$1
+docker push 274685854631.dkr.ecr.us-east-1.amazonaws.com/alfred:latest$1
