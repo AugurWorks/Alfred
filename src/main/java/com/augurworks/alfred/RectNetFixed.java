bb0f3e0f95d1ec05fdab97d04c63e54fc451fef2
TheConnMan <bcconn2112@gmail.com>
2016-07-30 14:23:11 -0400

    Remove TrainingStats private object from RectNetFixed

--- src/main/java/com/augurworks/alfred/RectNetFixed.java
+++ src/main/java/com/augurworks/alfred/RectNetFixed.java
@@ -41,6 +41,10 @@ public class RectNetFixed {
     protected FixedNeuron output;
     private TimingInfo timingInfo;
 
+    private TrainingStopReason trainingStopReason;
+    public boolean brokeAtLocalMax;
+    public BigDecimal maxScore = BigDecimal.valueOf(NEGATIVE_INFINITY);
+
     /**
      * Constructs a new RectNet with 10 inputs and 5 layers of network.
      */
@@ -360,18 +364,6 @@ public class RectNetFixed {
         return BigDecimal.valueOf(SIGMOID_ALPHA).multiply(last).multiply(oneMinusLast).multiply(desiredMinusLast);
     }
 
-    private class TrainingStats {
-        public TrainingStopReason stopReason;
-        public boolean brokeAtLocalMax;
-        public BigDecimal maxScore;
-
-        public TrainingStats() {
-            this.brokeAtLocalMax = false;
-            this.stopReason = TrainingStopReason.HIT_TRAINING_LIMIT;
-            this.maxScore = BigDecimal.valueOf(NEGATIVE_INFINITY);
-        }
-    }
-
     private void checkInterrupted() throws InterruptedException {
         if (Thread.interrupted()) {
             throw new InterruptedException("Job detected interrupt flag");
@@ -398,7 +390,6 @@ public class RectNetFixed {
             MDC.put("minTrainingRounds", netSpec.getMinTrainingRounds());
             this.timingInfo = TimingInfo.withDuration(trainingTimeLimitMillis);
             // Actually do the training part
-            TrainingStats trainingStats = new TrainingStats();
 
             int fileIteration;
             BigDecimal score = null;
@@ -416,7 +407,7 @@ public class RectNetFixed {
                 }
 
                 if (this.hasTimeExpired()) {
-                    trainingStats.stopReason = TrainingStopReason.OUT_OF_TIME;
+                    this.trainingStopReason = TrainingStopReason.OUT_OF_TIME;
                     break;
                 }
 
@@ -438,18 +429,18 @@ public class RectNetFixed {
                 if (BigDecimal.ZERO.min(
                         score.subtract(BigDecimal.valueOf(-1.0).multiply(netSpec.getPerformanceCutoff())))
                             .equals(BigDecimal.ZERO)) {
-                    trainingStats.stopReason = TrainingStopReason.HIT_PERFORMANCE_CUTOFF;
+                    this.trainingStopReason = TrainingStopReason.HIT_PERFORMANCE_CUTOFF;
                     break;
                 }
                 // if (score > maxScore)
                 //   ==> if (score - maxScore > 0)
                 //     ==> if (min(score - maxScore, 0) == 0)
-                if (BigDecimal.ZERO.min(score.subtract(trainingStats.maxScore)).equals(BigDecimal.ZERO)) {
-                    trainingStats.maxScore = score;
+                if (BigDecimal.ZERO.min(score.subtract(this.maxScore)).equals(BigDecimal.ZERO)) {
+                    this.maxScore = score;
                 } else if (fileIteration < netSpec.getMinTrainingRounds()) {
                     continue;
                 } else {
-                    trainingStats.brokeAtLocalMax = true;
+                    this.brokeAtLocalMax = true;
                     break;
                 }
 
@@ -458,9 +449,12 @@ public class RectNetFixed {
                 }
 
             }
+            if (this.trainingStopReason == null) {
+                this.trainingStopReason = TrainingStopReason.HIT_TRAINING_LIMIT;
+            }
             MDC.put("netScore", score.round(new MathContext(4)).toString());
             MDC.put("roundsTrained", fileIteration);
-            if (trainingStats.brokeAtLocalMax) {
+            if (this.brokeAtLocalMax) {
                 long timeExpired = System.currentTimeMillis() - this.timingInfo.getStartTime();
                 long timeRemaining = trainingTimeLimitMillis - timeExpired;
                 log.info("Retraining net from file {} with {} remaining.", name, TimeUtils.formatSeconds((int)timeRemaining/1000));
