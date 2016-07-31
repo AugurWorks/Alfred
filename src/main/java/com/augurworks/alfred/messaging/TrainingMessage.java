f58c291abfc67309c2544e53ad2c4b9e0e0b04dd
TheConnMan <bcconn2112@gmail.com>
2016-07-31 15:52:48 -0400

    Add metadata section to TrainingMessage

--- src/main/java/com/augurworks/alfred/messaging/TrainingMessage.java
+++ src/main/java/com/augurworks/alfred/messaging/TrainingMessage.java
@@ -1,20 +1,26 @@
 package com.augurworks.alfred.messaging;
 
 import com.augurworks.alfred.stats.TrainingStat;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class TrainingMessage implements Serializable {
+
+    TrainingMessage(String netId, String data, List<TrainingStat> trainingStats) {
+        this.netId = netId;
+        this.data = data;
+        this.trainingStats = trainingStats;
+    }
 
     private String netId;
     private String data;
 
+    private Map<String, String> metadata;
     private List<TrainingStat> trainingStats;
 }
