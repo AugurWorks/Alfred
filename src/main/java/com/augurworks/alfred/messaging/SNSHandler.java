f3e4b9507c8ca9ec0e6f471d8369084883301101
TheConnMan <bcconn2112@gmail.com>
2016-07-31 15:54:12 -0400

    Add SQS submission for finished jobs

--- src/main/java/com/augurworks/alfred/messaging/SNSHandler.java
+++ src/main/java/com/augurworks/alfred/messaging/SNSHandler.java
@@ -3,21 +3,28 @@ package com.augurworks.alfred.messaging;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+import com.amazonaws.services.sqs.AmazonSQSClient;
 import com.augurworks.alfred.RectNetFixed;
 import com.augurworks.alfred.server.AlfredWrapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
-import java.util.List;
 
 public class SNSHandler implements RequestHandler<SNSEvent, TrainingMessage> {
 
+    ObjectMapper mapper = new ObjectMapper();
+    AmazonSQSClient sqsClient = new AmazonSQSClient();
+
+    private static final String SQS_NAME_KEY = "sqsName";
+    private final Integer TRAINING_BUFFER_SEC = 5;
+
     public TrainingMessage handleRequest(SNSEvent snsEvent, Context context) {
-        ObjectMapper mapper = new ObjectMapper();
         try {
             TrainingMessage trainingMessage = mapper.readValue(snsEvent.getRecords().get(0).getSNS().getMessage(), TrainingMessage.class);
-            RectNetFixed rectNetFixed = AlfredWrapper.trainStatic(trainingMessage.getNetId(), trainingMessage.getData());
-            return new TrainingMessage(rectNetFixed.getName(), rectNetFixed.getAugout(), rectNetFixed.getTrainingStats());
+            RectNetFixed rectNetFixed = AlfredWrapper.trainStatic(trainingMessage.getNetId(), trainingMessage.getData(), context.getRemainingTimeInMillis() - 1000 * TRAINING_BUFFER_SEC);
+            TrainingMessage outputMessage = new TrainingMessage(rectNetFixed.getName(), rectNetFixed.getAugout(), rectNetFixed.getTrainingStats());
+            sqsClient.sendMessage(trainingMessage.getMetadata().get(SQS_NAME_KEY), mapper.writeValueAsString(outputMessage));
+            return outputMessage;
         } catch (IOException e) {
             e.printStackTrace();
             return null;
--- src/main/java/com/augurworks/alfred/server/AlfredWrapper.java
+++ src/main/java/com/augurworks/alfred/server/AlfredWrapper.java
@@ -122,17 +122,16 @@ public class AlfredWrapper {
         return null;
     }
 
-    public static RectNetFixed trainStatic(String netId, String augtrain) {
+    public static RectNetFixed trainStatic(String netId, String augtrain, Integer timeoutMillis) {
         ScaleFunctionType scaleFunctionType = ScaleFunctionType.SIGMOID;
-        Integer timeout = 2 * 60;
 
         MDC.put("netId", netId);
-        MDC.put("trainingTimeLimitSec", timeout);
+        MDC.put("trainingTimeLimitSec", timeoutMillis / 1000);
         MDC.put("scaleFunctionType", scaleFunctionType.name());
 
         try {
             List<String> lines = Splitter.on("\n").splitToList(augtrain);
-            RectNetFixed net = new RectNetFixed(netId, lines, scaleFunctionType).train(timeout * 1000, 5);
+            RectNetFixed net = new RectNetFixed(netId, lines, scaleFunctionType).train(timeoutMillis, 5);
             return net;
         } catch (Exception t) { }
         return null;
