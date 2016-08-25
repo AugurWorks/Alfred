2d2e40dac942e30c606ea8406e47b8165d0421d9
TheConnMan <bcconn2112@gmail.com>
2016-08-25 17:35:01 -0400

    Change BigDecimal precision sigmoid calculations to doubles precision
    Difference in exponential calculations began to differ between
    BigDecimal and double around the 17th significant digit.

--- src/main/java/com/augurworks/alfred/util/BigDecimals.java
+++ src/main/java/com/augurworks/alfred/util/BigDecimals.java
@@ -15,30 +15,6 @@ public class BigDecimals {
         // utility class
     }
 
-    public static BigDecimal exp(BigDecimal value) {
-        return exp(value, DEFAULT_PRECISION);
-    }
-
-    public static BigDecimal exp(BigDecimal value, int precision) {
-        MathContext mc = new MathContext(precision + 2);
-        BigDecimal retVal = BigDecimal.ZERO;
-        BigDecimal oldVal;
-        int n = 0;
-        do {
-            oldVal = retVal;
-            retVal = retVal.add(value.pow(n, mc).divide(factorial(n), mc), mc);
-            n++;
-        } while (!retVal.equals(oldVal));
-        return retVal.round(new MathContext(precision));
-    }
-
-    public static BigDecimal factorial(long value) {
-        BigDecimal retVal = BigDecimal.ONE;
-        for (long i = value; i > 0; i--)
-            retVal = retVal.multiply(new BigDecimal(i));
-        return retVal;
-    }
-
     /**
      * Performs the sigmoid function on an input height = 1 / (1 + exp(-alpha*depth))
      * Used internally in getOutput method. Alpha is set to 3 currently.
@@ -49,12 +25,8 @@ public class BigDecimals {
      */
     public static BigDecimal sigmoid(BigDecimal input) {
         //	1.0 / (1.0 + Math.exp(-3.0 * input));
-        return BigDecimal.ONE.divide(
-                BigDecimal.ONE.add(
-                        BigDecimals.exp(BigDecimal.valueOf(-RectNetFixed.SIGMOID_ALPHA).multiply(input,
-                                BigDecimals.MATH_CONTEXT)),
-                        BigDecimals.MATH_CONTEXT),
-                BigDecimals.MATH_CONTEXT);
+        double exponential = Math.exp(-RectNetFixed.SIGMOID_ALPHA * input.doubleValue());
+        return new BigDecimal(1.0 / (1.0 + exponential));
     }
 
 }
